8b9b60cae1689d0665bcc3c88071ae0c4683cd03
pubestpubest <pubestpubest@gmail.com>
2023-07-17 16:05:42 +0700

    footer name

--- src/app/page.js
+++ src/app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
       {/* //footer section */}
       <div className="mt-3">
         <p className="text-secondary text-center">
-          copyright © 2023 Chayanin Suatap 12345678
+          copyright © 2023 Pubest Ruengkum 650610798
         </p>
       </div>
     </div>
